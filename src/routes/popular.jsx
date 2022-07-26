616066493a4251ebc097121596ec18d77abc4354
Sagar <acharyasaagar@gmail.com>
2022-07-26 22:02:55 +0300

    Adds Popular View

--- src/routes/popular.jsx
+++ src/routes/popular.jsx
@@ -2,15 +2,22 @@ import React from 'react'
 import PopularView from '../views/popular'
 import { fetchMovies } from '../apis/movie-api'
 import { useAsync } from '../hooks/use-async'
+import { getMovieData } from '../utils'
 
-const fetchPopularMovies = () => fetchMovies('/movie/popular')
+const fetchPopularMovies = () => {
+  return fetchMovies('/movie/popular').then((response) => {
+    console.log(response.results)
+    return response.results.map(getMovieData)
+  })
+}
 
 const PopularMoviesRoute = () => {
   const { data: movies, error, status } = useAsync(fetchPopularMovies)
-
   if (status === 'pending') return <p>pending</p>
   if (status === 'error') return <p>{error}</p>
-  if (status === 'success' && movies?.length) return <PopularView />
+  if (status === 'success' && movies?.length) {
+    return <PopularView movies={movies} />
+  }
   return null
 }
 
--- src/views/popular/Popular.jsx
+++ src/views/popular/Popular.jsx
@@ -1,7 +1,33 @@
 import React from 'react'
+import MovieCard from '../../components/MovieCard'
+import Grid from '@material-ui/core/Grid'
+import Box from '@material-ui/core/Box'
+import Typography from '@material-ui/core/Typography'
 
-const Popular = () => {
-  return <div>Popular</div>
+const Popular = ({ movies }) => {
+  return (
+    <>
+      <Box pt={2} pb={6}>
+        <Typography variant="h1">Popular movies</Typography>
+      </Box>
+      <Grid container spacing={4} wrap="wrap" justify="space-between">
+        {movies.map((movie) => {
+          return (
+            <Grid item key={movie.title}>
+              <MovieCard
+                key={movie.title}
+                title={movie.title}
+                subtitle={movie.subtitle}
+                imgSrc={movie.backdrop}
+                trendingIndex={1}
+                trailerButton={true}
+              />
+            </Grid>
+          )
+        })}
+      </Grid>
+    </>
+  )
 }
 
 export default Popular
