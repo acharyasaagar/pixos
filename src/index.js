9a739dc6816e0ea521f2882c5d22206d1636859b
Sagar <acharyasaagar@gmail.com>
2022-07-26 21:25:11 +0300

    Adds Suspense

--- src/index.js
+++ src/index.js
@@ -5,11 +5,15 @@ import theme from './theme'
 import { ThemeProvider } from '@material-ui/core/styles'
 import { BrowserRouter } from 'react-router-dom'
 
+const SuspenseFallback = () => <p>something went wrong</p>
+
 ReactDOM.render(
   <ThemeProvider theme={theme}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <React.Suspense fallback={<SuspenseFallback />}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </React.Suspense>
   </ThemeProvider>,
   document.getElementById('root')
 )
