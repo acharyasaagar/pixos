b84d6111b913e9dc84ef1f522be35680028d973e
Sagar <acharyasaagar@gmail.com>
2022-07-26 21:19:21 +0300

    Closes drawer on route change

--- src/layouts/components/Menu/MenuDrawer.js
+++ src/layouts/components/Menu/MenuDrawer.js
@@ -1,16 +1,20 @@
 import React, { useState } from 'react'
-
+import Menu from './Menu'
 import Drawer from '@material-ui/core/Drawer'
 import MenuIcon from '@material-ui/icons/Menu'
 import IconButton from '@material-ui/core/IconButton'
-
-import Menu from './Menu'
+import { useLocation } from 'react-router-dom'
 
 const MenuDrawer = () => {
+  const { pathname } = useLocation()
   const [drawerOpen, setDrawerOpen] = useState(false)
-
   const openDrawer = () => setDrawerOpen(true)
   const closeDrawer = () => setDrawerOpen(false)
+
+  React.useEffect(() => {
+    if (drawerOpen) closeDrawer()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [pathname])
 
   return (
     <div>
