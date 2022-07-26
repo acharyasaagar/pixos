e389fc1e52b8a34d088d093abd0e08103a631ada
Sagar <acharyasaagar@gmail.com>
2022-07-26 21:20:51 +0300

    Adds BrowserRouter to app

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
-import { ThemeProvider } from '@material-ui/core/styles'
-
 import App from './App'
 import theme from './theme'
+import { ThemeProvider } from '@material-ui/core/styles'
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.render(
   <ThemeProvider theme={theme}>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </ThemeProvider>,
   document.getElementById('root')
 )
